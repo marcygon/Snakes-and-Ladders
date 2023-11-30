dd7a7f57264b8bbee2ee7d3ad346984a5d94c49f
marcygon <marciasofia49@gmail.com>
2023-11-30 10:02:10 +0100

    first commit

---
+++ src/test/java/com/marcygon/snakesandladders/SnakesAndLaddersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcygon.snakesandladders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnakesAndLaddersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
